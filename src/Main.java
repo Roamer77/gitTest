c824d6bd2260aa4350be2359d276e0f119baf4bb
Roamer77 <jatgaret@gmail.com>
2020-01-22 14:52:16 +0300

    камит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello GIT");
+        System.out.println("Новая фича введене");
+        System.out.println("И ещё одну новую фича введене");
+        System.out.println("testBranch");
+        System.out.println("Hello GIT");
+        System.out.println("Новая фича введене");
+        System.out.println("И ещё одну новую фича введене");
+        System.out.println("testBranch");
     }
 }
